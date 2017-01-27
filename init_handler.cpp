eaf3397e2a71bf1cf18b33da8826b5f5a0381836
nikbsf <nikbsf@gmail.com>
2017-01-28 01:17:08 +0500

    key setting

--- init_handler.cpp
+++ init_handler.cpp
@@ -1,13 +1,20 @@
 #include "stdafx.h"
 #include "shared.h"
 
+extern advconfig_string_factory cfg_key_format;
+
 class init_handler : public init_stage_callback {
 public:
-	void on_init_stage(t_uint32 stage) {
+	void on_init_stage(t_uint32 stage) override
+	{
 		if (stage != init_stages::after_library_init) 
 			return;
 
 		console::print(COMPONENT_NAME ": on_init()");
+
+		pfc::string8 key;
+		cfg_key_format.get(key);
+		console::formatter() << "key: " << key;
 	}
 };
 
