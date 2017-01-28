d27dc65f464d433eefd966e904cd402edd0c6f31
nikbsf <nikbsf@gmail.com>
2017-01-29 03:48:44 +0500

    cache titleformat script

--- key_provider.cpp
+++ key_provider.cpp
@@ -4,8 +4,7 @@
 
 extern advconfig_string_factory cfg_key_format;
 
-
-pfc::string8 key_provider::get_key(metadb_handle_ptr item) const {
+pfc::string8 key_provider::get_key(metadb_handle_ptr item) {
 	pfc::string8 result;
 
 	auto key_format_script = get_key_format_script();
@@ -13,11 +12,19 @@ pfc::string8 key_provider::get_key(metadb_handle_ptr item) const {
 	return result;
 }
 
-titleformat_object::ptr key_provider::get_key_format_script() const {
+titleformat_object::ptr key_provider::get_key_format_script() {
 	pfc::string8 key_format;
 	cfg_key_format.get(key_format);
-
+	
 	titleformat_object::ptr result;
-	if (static_api_ptr_t<titleformat_compiler>()->compile(result, key_format))
-		return result;
+
+	if (m_key_format_script_ptr.is_empty() || m_key_format != key_format) {
+		
+		if (static_api_ptr_t<titleformat_compiler>()->compile(result, key_format)) {
+			m_key_format = key_format;
+			m_key_format_script_ptr = result;
+		}
+	}
+
+	return m_key_format_script_ptr;
 }
--- key_provider.h
+++ key_provider.h
@@ -1,8 +1,10 @@
 #pragma once
 
-class key_provider : public service_base {
-	titleformat_object::ptr get_key_format_script() const;
+class key_provider : public service_base {	
+	pfc::string8 m_key_format;
+	titleformat_object::ptr m_key_format_script_ptr;
+	titleformat_object::ptr get_key_format_script();
 
-public:
-	pfc::string8 get_key(metadb_handle_ptr item) const;
+public:	
+	pfc::string8 get_key(metadb_handle_ptr item);
 };
