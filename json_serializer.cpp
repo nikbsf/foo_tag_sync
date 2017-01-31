804ffd35a414ee21aae7e3ac6abc752fb7cc4d5f
nikbsf <nikbsf@gmail.com>
2017-01-31 05:47:44 +0500

    real JSON serializer

--- json_serializer.cpp
+++ json_serializer.cpp
@@ -1,13 +1,25 @@
 #include "stdafx.h"
 
+#include "rapidjson/document.h"
+#include "rapidjson/prettywriter.h"
+
 #include "json_serializer.h"
 
+using namespace rapidjson;
+
 pfc::string8 json_serializer::serialize(pfc::string8 key, tags_list_t item_tags_data) {
-	pfc::string8 result;
+	StringBuffer buffer;
+	PrettyWriter<StringBuffer> writer(buffer);
+	Document document;
+
+	document.SetObject();
 	for (auto it = item_tags_data.begin(); it != item_tags_data.end(); ++it) {
-		// dummy, todo
-		result.add_string(it->first);
-		result.add_string(it->second);
+		Value tag_name (kStringType), tag_value;
+		tag_name.SetString(it->first.get_ptr(), it->first.get_length());
+		tag_value.SetString(it->second.get_ptr(), it->second.get_length());
+		document.AddMember(tag_name, tag_value, document.GetAllocator());
 	}
-	return result;
+	
+	document.Accept(writer);
+	return buffer.GetString();
 }
