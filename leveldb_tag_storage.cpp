944e47a5e31a41888b8349733e740a2cae21d2c1
nikbsf <nikbsf@gmail.com>
2017-01-31 06:51:43 +0500

    LevelDB batch write

--- leveldb_tag_storage.cpp
+++ leveldb_tag_storage.cpp
@@ -4,6 +4,7 @@
 
 #include "leveldb/options.h"
 #include "leveldb/db.h"
+#include "leveldb/write_batch.h"
 
 #include "leveldb_tag_storage.h"
 
@@ -19,10 +20,14 @@ void leveldb_tag_storage::save_tags(serialized_tags_dict_t serialized_tags_dict)
 	if (!status.ok())
 		return;
 
-	leveldb::WriteOptions writeOptions;
+	leveldb::WriteOptions write_options;
+	leveldb::WriteBatch batch;
+
 	for (auto it = serialized_tags_dict.begin(); it != serialized_tags_dict.end(); ++it) {
 		auto key = it->first;
 		auto serialized_tags = it->second;
-		dbptr->Put(writeOptions, key.get_ptr(), serialized_tags.get_ptr());
+		batch.Put(key.get_ptr(), serialized_tags.get_ptr());
 	}
+
+	dbptr->Write(write_options, &batch);
 }
