425a529394823d83f60f94c2504e18448ca48703
nikbsf <nikbsf@gmail.com>
2017-02-02 16:04:58 +0500

    rename

--- edit_handler.cpp
+++ edit_handler.cpp
@@ -2,11 +2,11 @@
 
 #include "tag_exporter.h"
 
-class metadb_io_edit_callback_handler : public metadb_io_edit_callback {
+class edit_handler : public metadb_io_edit_callback {
 	service_ptr_t<tag_exporter> tag_exporter_service;
 
 public:
-	metadb_io_edit_callback_handler() : tag_exporter_service(new service_impl_t<tag_exporter>()) {
+	edit_handler() : tag_exporter_service(new service_impl_t<tag_exporter>()) {
 	}
 
 	void on_edited(metadb_handle_list_cref items, t_infosref before, t_infosref after) override {
@@ -14,4 +14,4 @@ public:
 	}
 };
 
-static service_factory_single_t<metadb_io_edit_callback_handler> g_edit_handler_factory;
+static service_factory_single_t<edit_handler> g_edit_handler_factory;
--- tag_exporter.cpp
+++ tag_exporter.cpp
@@ -11,12 +11,12 @@ bool tag_exporter::is_empty(const tags_list_t& tags) {
 }
 
 void tag_exporter::export_tags(metadb_handle_list_cref items, const pfc::list_base_const_t<const file_info*>* file_infos) {
-	std::map<pfc::string8, pfc::string8> tags_by_keys;
+	serialized_tags_dict_t serialized_tags;
 
 	auto exported_items_count = 0;
 	for (auto i = 0; i < items.get_count(); ++i) {
 		auto item_metadb_handle = items[i];
-		const file_info* item_file_info = file_infos != NULL ? (*file_infos)[i] : &items[i]->get_info_ref()->info();
+		auto item_file_info = file_infos != NULL ? (*file_infos)[i] : &items[i]->get_info_ref()->info();
 
 		auto key = key_provider_service->get_key(item_metadb_handle);
 		auto tags = tag_extractor_service->get_tags_with_values(item_file_info);
@@ -24,10 +24,10 @@ void tag_exporter::export_tags(metadb_handle_list_cref items, const pfc::list_ba
 		if (is_empty(tags))
 			continue;
 
-		tags_by_keys[key] = json_serializer_service->serialize(key, tags);
+		serialized_tags[key] = json_serializer_service->serialize(key, tags);
 		++exported_items_count;
 	}
-	tag_storage_service->save_tags(tags_by_keys);
+	tag_storage_service->save_tags(serialized_tags);
 
 	console::printf(COMPONENT_NAME ": exported %d of %d", exported_items_count, items.get_count());
 }
