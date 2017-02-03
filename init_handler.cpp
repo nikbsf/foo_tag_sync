df0fabbfa5fd9bfd5b5cffad78dfa3835a36eca3
nikbsf <nikbsf@gmail.com>
2017-02-03 11:37:24 +0500

    fetch interval setting

--- init_handler.cpp
+++ init_handler.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
+
 #include "tag_importer.h"
 
 extern service_ptr_t<tag_importer> g_tag_importer;
+extern advconfig_integer_factory cfg_fetch_interval_seconds;
 
 class updates_check_timer : public system_time_callback_impl {
 public:
@@ -15,7 +17,8 @@ static updates_check_timer g_updates_check_timer;
 class init_handler : public initquit {
 public:
 	void on_init() override {
-		g_updates_check_timer.initialize_timer(system_time_periods::second * 5);
+		auto fetch_interval  = cfg_fetch_interval_seconds.get();
+		g_updates_check_timer.initialize_timer(fetch_interval * system_time_periods::second);
 	}
 
 	void on_quit() override {
